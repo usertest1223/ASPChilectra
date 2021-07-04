cad549dab1bcbf0752fe4c65cd5917ee41650409
VICTOR ARMANDO GOMEZ MORENO <victor_armando12@hotmail.com>
2021-07-04 00:32:45 -0500

    Cambios en el boton de calcular para se envien los datos a la BD cuando el boton es presionado.

--- ASPChilectra/FRMboletachilectra.aspx.cs
+++ ASPChilectra/FRMboletachilectra.aspx.cs
@@ -25,6 +25,47 @@ namespace ASPChilectra
             txtfechaPago.Text = Calculo.fechapago(cbtipificacion.SelectedValue);
             txtvalorUF.Text = Calculo.valorUF(txtfechaPago.Text).ToString();
             txtvalorPagar.Text = (Calculo.ValorKW*Calculo.factorclimatico(Calculo.mes(cbtipificacion.SelectedValue))*Calculo.ConsumoActual()*Calculo.valorUF(txtfechaPago.Text)).ToString();
+
+            //agregamos los datos del cliente a la BD de la tabla Cliente
+            Cliente objcliente = new Cliente("", "", "", "", "", 0, 0);
+            objcliente.Rut = this.control.ConseguirRut();
+            objcliente.Nombre = txtNombre.Text;
+            objcliente.Direccion = txtDireccion.Text;
+            objcliente.Tipificacion = cbtipificacion.SelectedValue;
+            objcliente.Comuna = txtComuna.Text;
+            objcliente.Lecturaactual = int.Parse(txtLecturaActual.Text);
+            objcliente.Lecturaanterior = int.Parse(txtLecturaAnterior.Text);
+
+            objcliente.agregar();
+
+
+            //agregaos los datos de consumo a la tabla Consumo
+            Consumo con = new Consumo("", 0.0, new DateTime(), 0);
+            string fechPago = this.txtfechaPago.Text;
+            DateTime oDate1 = Convert.ToDateTime(fechPago);
+            int mes = oDate1.Month;
+            con.Fecha = oDate1;
+            con.ConsumoAnterior = double.Parse(this.txtconsumoAnterior.Text);
+            con.Tipificacion = cbtipificacion.SelectedValue;
+            con.MesPago = mes;
+            con.agregar();
+
+
+            //agregamos los datos de la Boleta  a la BD de la tabla Boleta
+
+            Boleta bol = new Boleta("", new DateTime(), 0.0, 0.0, 0.0, 0.0);
+            bol.Rut = this.control.ConseguirRut();
+            string iDate = this.txtfechaPago.Text;
+            DateTime oDate2 = Convert.ToDateTime(iDate);
+            bol.Fecha = oDate2;
+            bol.ValorUf = double.Parse(this.txtvalorUF.Text);
+            bol.ConsumoActual = double.Parse(this.txtconsumoActual.Text);
+            bol.ConsumoAnterior = double.Parse(this.txtconsumoAnterior.Text);
+            bol.TotalaApagar = double.Parse(this.txtvalorPagar.Text);
+
+            bol.agregar();
+
+
         }
     }
    
